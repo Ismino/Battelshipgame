1445d9d5bd43daf56ec7ba34c15335daebfd11e9
Ismino <denise_cigel@hotmail.com>
2023-05-09 16:33:58 +0000

    Update some functions and add a play function

--- run.py
+++ run.py
@@ -9,12 +9,15 @@ def create_board():
         board.append([' '] * 6)
     return board
 
-def print_board(board):
+def print_board(board, hide_ships=True):
     """
-    Printing the board
+    Printing the board. If hide_ships is True, ship locations are hidden.
     """
     print("|" + "|".join(str(i) for i in range(1, 7)))
     for i, row in enumerate(board):
+        if hide_ships:
+            # Replace ship characters with spaces
+            row = [' ' if cell in ['B', 'C', 'D'] else cell for cell in row]
         print(str(i + 1) + "|" + "|".join(row) + "|")
 
 def ships_placement(board, ships):
@@ -47,7 +50,6 @@ def ships_placement(board, ships):
                     for i in range(ships[ship]):
                         board[x+i][y] = ship[0].upper()
                     onboard = True
-
 def get_guess():
     """
     Getting the guess from the user and returning as a tuple.
@@ -61,15 +63,30 @@ def get_guess():
             if row < 1 or row > 6 or col < 1 or col > 6:
                 print("Invalid input. Please enter values between 1 and 6.")
             else:
-                return (row, col)
+                return (row, col) 
         except ValueError:
-            print("Invalid input. Please enter values in the format 'row,column'.")
+            print("Invalid input. Please enter values in the format 'row,column'.") 
+def check_guess(guess, board):
+    row, col = guess
+    if board[row-1][col-1] != ' ':
+        board[row-1][col-1] = 'X'
+        print("Hit!")
+        return True
+    else:
+        board[row-1][col-1] = 'O'
+        print("Miss!")
+        return False
 
-def main():
+def play_battleship():
     board = create_board()
-    print_board(board)
     ships = {'battleship': 4, 'cruiser': 3, 'destroyer': 2}
     ships_placement(board, ships)
-    get_guess()
-
-main()
+    print("Let's play Battleship!")
+    for i in range(15):
+        print("Round", i+1)
+        print_board(board, hide_ships=True)
+        guess = get_guess()
+        check_guess(guess, board)
+    print("Game over!")
+    
+play_battleship()
